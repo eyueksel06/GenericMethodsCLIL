6b5e0f9fb980eb17a856400c5b7cc2df1e457533
Yueksel Eren Nazim <eyueksel@student.tgm.ac.at>
2024-02-17 19:29:55 +0100

    commit 02: Programm is done inclusive the Test class and the comments are complete

--- src/eyueksel/GenericUtils.java
+++ src/eyueksel/GenericUtils.java
@@ -1,14 +1,40 @@
 package eyueksel;
-
 /**
- *
+ * Class GenericUtils with two methods
+ * @author Eren Nazim Yüksel
+ * @version 2024-02-17
  */
 public class GenericUtils {
+    /**
+     * A method that flips the array (so that the first element is at the last and the last element first)
+     * The swapping happens in-place, so no new array is created.
+     * @param array
+     * @param <T>
+     */
     public static <T> void reverse (T[] array){
-
+        for(int i = 0; i < array.length/2; i++){
+            if(array[i] != null){
+                T temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
+            }
+        }
     }
+    /**
+     * The same function as the reverse method but only with numbers and returns the sum of the numbers
+     * in the array
+     * @param array
+     * @return sum
+     * @param <T>
+     */
     public static <T extends Number> double sum (T[] array){
-        double number = 0;
-        return number;
+        double sum = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] != null){
+                sum += array[i].doubleValue();  // If you try to cast the value of the index with (double) it does not work so u have to use the method doubleValue() that is excplicitly created for such a case
+            }
+        }
+        GenericUtils.reverse(array);
+        return sum;
     }
 }
--- src/eyueksel/Main.java
+++ src/eyueksel/Main.java
@@ -1,7 +1,19 @@
 package eyueksel;
 
+/**
+ * MAin Class to test the GenericUtlis Class
+ * @author Eren Nazim Yüksel
+ * @version 2024-02-17
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] stringArray = {"a", "b", "c", "d"};
+        Double[] intArray = {1.0, 2.4, 3.5, 4.9, 5.2};  // Integer or any type of Number Objects works aswell
+
+        GenericUtils.reverse(stringArray);
+        System.out.println(stringArray[0]+", "+stringArray[1]+", "+stringArray[2]+", "+stringArray[3]);
+
+        double sum = GenericUtils.sum(intArray);
+        System.out.println(sum);
     }
 }
