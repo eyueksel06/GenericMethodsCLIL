e4af98a5c1197792e3192b69c89382dff1e66092
Yueksel Eren Nazim <eyueksel@student.tgm.ac.at>
2024-02-17 14:13:41 +0100

    commit 01: Set up Git and created the Classes and the prototype of the methods

---
+++ src/eyueksel/GenericUtils.java
@@ -0,0 +1,14 @@
+package eyueksel;
+
+/**
+ *
+ */
+public class GenericUtils {
+    public static <T> void reverse (T[] array){
+
+    }
+    public static <T extends Number> double sum (T[] array){
+        double number = 0;
+        return number;
+    }
+}
